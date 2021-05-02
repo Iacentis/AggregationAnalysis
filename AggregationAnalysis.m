edd55b2f3c24ea347900ec76f79c33b6d64f5a06
Iacentis <CarlJFandersson@gmail.com>
2021-05-02 02:00:20 +0200

    Function Output fixed, added RMS

--- AggregationAnalysis.m
+++ AggregationAnalysis.m
@@ -1,11 +1,10 @@
-function [AggregationScores,Plots,GroupingData] = AggregationAnalysis(Uint8ImageData,AggregationSquareSize)
+function [AggregationScores,Plots,GroupingData,RootMeanSquares] = AggregationAnalysis(Uint8ImageData,AggregationSquareSize)
 % Uint8ImageData: Image Data, assumed to be in Uint8 format (may work for
 % other formats as well, but not tested
 % AggregationSquareSize: Size of the square to select aggregation data from
 % in pixels, integer
 
 DoubleImageData = double(Uint8ImageData);
-%DoubleImageData = DoubleImageData./max(DoubleImageData(:));
 Delta = DoubleImageData(2:end,:)-DoubleImageData(1:end-1,:); %dI / dx
 Delta = Delta(:,2:end)-Delta(:,1:end-1); % dI^2 / dx dy
 SquareData = zeros(size(Delta,1),size(Delta,2),(AggregationSquareSize*2+1)^2);
@@ -28,17 +27,21 @@ xMeanDeviation = xMeanDeviation./max(xMeanDeviation);
 yMeanDeviation = yMeanDeviation-min(yMeanDeviation);
 yMeanDeviation = yMeanDeviation./max(yMeanDeviation);
 
-xAggregationScore = 1./sum(diff([0,find(xMeanDeviation < mean(xMeanDeviation))])>1);
-yAggregationScore = 1./sum(diff([0;((find(yMeanDeviation < mean(yMeanDeviation))))])>1);
+GroupingData{1} = DeviationData;
+GroupingData{2} = xMeanDeviation;
+GroupingData{3} = yMeanDeviation;
+AggregationScores(1) = 1./sum(diff([0,find(xMeanDeviation < mean(xMeanDeviation))])>1);
+AggregationScores(2) = 1./sum(diff([0;((find(yMeanDeviation < mean(yMeanDeviation))))])>1);
+RootMeanSquares(1) = sqrt(sum(xMeanDeviation.^2)./numel(xMeanDeviation));
+RootMeanSquares(2) = sqrt(sum(yMeanDeviation.^2)./numel(yMeanDeviation));
 
-
-%%%%%% Plot Generation
+%%%%%%% Plot Generation
 subplot(1,3,1)
 hold off
-imagesc(Uint8ImageData) 
+Plots(1) = imagesc(Uint8ImageData);
 hold on
-plot3(1:numel(xMeanDeviation),zeros(size(xMeanDeviation)),xMeanDeviation,'LineWidth',2)
-plot3(zeros(size(yMeanDeviation))+numel(xMeanDeviation),1:numel(yMeanDeviation),yMeanDeviation,'LineWidth',2)
+Plots(2) = plot3(1:numel(xMeanDeviation),zeros(size(xMeanDeviation)),xMeanDeviation,'LineWidth',2);
+Plots(3) = plot3(zeros(size(yMeanDeviation))+numel(xMeanDeviation),1:numel(yMeanDeviation),yMeanDeviation,'LineWidth',2);
 xlabel('X')
 ylabel('Y')
 axis([0,numel(xMeanDeviation),0,numel(yMeanDeviation),0,max(max(yMeanDeviation),max(xMeanDeviation))])
@@ -46,22 +49,24 @@ view(3)
 
 subplot(1,3,2)
 hold off
-plot(xMeanDeviation)
+Plots(4) = plot(xMeanDeviation);
 hold on
-plot(xMeanDeviation.*0+mean(xMeanDeviation))
+Plots(5) = plot(xMeanDeviation.*0+mean(xMeanDeviation));
+Plots(6) = plot(xMeanDeviation.*0+RootMeanSquares(1));
 xlabel('X')
 ylabel('Relative Intensity')
-title(sprintf('X-Aggregation Score = %g',xAggregationScore))
+title(sprintf('X-Aggregation Score = %g',AggregationScores(1)))
 axis([0,numel(xMeanDeviation),0,max(xMeanDeviation)])
 
 subplot(1,3,3)
 hold off
-plot(yMeanDeviation)
+Plots(7) = plot(yMeanDeviation);
 hold on
-plot(yMeanDeviation.*0+mean(yMeanDeviation))
+Plots(8) = plot(yMeanDeviation.*0+mean(yMeanDeviation));
+Plots(9) = plot(yMeanDeviation.*0+RootMeanSquares(2));
 xlabel('Y')
 ylabel('Relative Intensity')
-title(sprintf('Y-Aggregation Score = %g',yAggregationScore))
+title(sprintf('Y-Aggregation Score = %g',AggregationScores(2)))
 axis([0,numel(yMeanDeviation),0,max(yMeanDeviation)])
 
 end
